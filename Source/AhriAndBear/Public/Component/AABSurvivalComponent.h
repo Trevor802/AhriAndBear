50709c3d2139e94a60d8a747580584809b9998d8
Joseph Newman <paradoxbuilder@gmail.com>
2020-10-13 14:15:39 -0600

    Added basic stats

--- Source/AhriAndBear/Public/Component/AABSurvivalComponent.h
+++ Source/AhriAndBear/Public/Component/AABSurvivalComponent.h
@@ -16,6 +16,37 @@ public:
 	// Sets default values for this component's properties
 	UAABSurvivalComponent();
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Health")
+	float MaxHealth = 100.f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Survival | Health")
+	float CurrentHealth;
+	UPROPERTY(EditAnywhere, BlueprintReadWhite, Category = "Character | Survival | Health")
+	float HealthChangeRate = -1.f;
+
+	// Thirst-related stats
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Thirst")
+	float MaxThirst = 100.f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Survival | Thirst")
+	float CurrentThirst;
+	UPROPERTY(EditAnywhere, BlueprintReadWhite, Category = "Character | Survival | Thirst")
+	float ThirstChangeRate = -1.f;
+
+	// Hunger-related stats
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Hunger")
+	float MaxFullness = 100.f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Survival | Hunger")
+	float CurrentFullness;
+	UPROPERTY(EditAnywhere, BlueprintReadWhite, Category = "Character | Survival | Hunger")
+	float FullnessChangeRate = -1.f;
+
+	// Warmth-related stats
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Warmth")
+	float MaxWarmth = 100.f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Survival | Warmth")
+	float CurrentWarmth;
+	UPROPERTY(EditAnywhere, BlueprintReadWhite, Category = "Character | Survival | Warmth")
+	float WarmthChangeRate = -1.f;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
