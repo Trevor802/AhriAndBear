c48fa419d5186c3f5c7b472448213d45706ab453
Xiubo Zhao <906672365@qq.com>
2020-10-18 13:42:23 +0800

    [Scent] Init files

---
+++ Source/AhriAndBear/Environments/ABScentTrail.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABScentTrail.h"
+
+// Sets default values
+AABScentTrail::AABScentTrail()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AABScentTrail::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AABScentTrail::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
