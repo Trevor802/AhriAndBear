4e01e2e8fb8cadaafb39cf19e85c7a764992600b
Xiubo Zhao <906672365@qq.com>
2020-10-18 15:15:11 +0800

    [Scent] Basically implemented - delegate needed

--- Source/AhriAndBear/Environments/ABScentTrail.cpp
+++ Source/AhriAndBear/Environments/ABScentTrail.cpp
@@ -2,13 +2,20 @@
 
 
 #include "ABScentTrail.h"
+#include "Components/SphereComponent.h"
+#include "Components/SplineComponent.h"
+#include "NiagaraComponent.h"
+#include "NiagaraSystem.h"
+#include "NiagaraFunctionLibrary.h"
 
 // Sets default values
 AABScentTrail::AABScentTrail()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+ 
+	spline = CreateDefaultSubobject<USplineComponent>(TEXT("Spline"));
+	RootComponent = spline;
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +23,7 @@ void AABScentTrail::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	InitTrail();
 }
 
 // Called every frame
@@ -25,3 +33,26 @@ void AABScentTrail::Tick(float DeltaTime)
 
 }
 
+void AABScentTrail::InitTrail()
+{
+	// Calculate the positions along the spline to generate scent Niagara System
+	int niagaraQuantity = (int)floor(spline->GetSplineLength() / Interval);
+
+	// Generate Niagara System along the spline
+	for (size_t i = 0; i < niagaraQuantity; i++)
+	{
+		float distance = Interval * i;
+		FVector position = spline->GetLocationAtDistanceAlongSpline(distance, ESplineCoordinateSpace::World);
+		UNiagaraComponent* tempSystem = UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), TemplateSystem, position, FRotator::ZeroRotator, false);
+		tempSystem->Deactivate();
+		systemArray.Add(tempSystem);
+	}
+}
+
+void AABScentTrail::ShowTrail()
+{
+	for (int i = 0; i < systemArray.Num(); i++)
+	{
+		systemArray[i]->Activate();
+	}
+}
--- Source/AhriAndBear/Environments/ABScentTrail.h
+++ Source/AhriAndBear/Environments/ABScentTrail.h
@@ -6,21 +6,37 @@
 #include "GameFramework/Actor.h"
 #include "ABScentTrail.generated.h"
 
+class USplineComponent;
+class USphereComponent;
+class UNiagaraComponent;
+class UNiagaraSystem;
 UCLASS()
 class AHRIANDBEAR_API AABScentTrail : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning Property")
+	int Interval;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning Property")
+	UNiagaraSystem* TemplateSystem;
+
 	// Sets default values for this actor's properties
 	AABScentTrail();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	UPROPERTY()
+	USplineComponent* spline;
 
+	TArray<UNiagaraComponent*> systemArray;
+
+	void InitTrail();
+	void ShowTrail();
+	void HideTrail();
 };
