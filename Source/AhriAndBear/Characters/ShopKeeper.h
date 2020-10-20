60b809467e4e14ef475b17642b11d8cb4c5e32bb
Rud156 <rudrangshu_1997@yahoo.com>
2020-10-19 20:47:40 -0600

    Converted AI Modules to C++

---
+++ Source/AhriAndBear/Characters/ShopKeeper.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShopKeeper.generated.h"
+
+UCLASS()
+class AHRIANDBEAR_API AShopKeeper : public ACharacter
+{
+	GENERATED_BODY()
+
+		UPROPERTY(Category = "Shopkeeper", VisibleDefaultsOnly)
+		class UBoxComponent* Box;
+
+	UPROPERTY(Category = "Shopkeeper", VisibleDefaultsOnly)
+		class UBoxComponent* Box1;
+
+	UPROPERTY(Category = "Shopkeeper", VisibleDefaultsOnly)
+		class USkeletalMeshComponent* SkeletalMesh;
+
+public:
+	// Sets default values for this character's properties
+	AShopKeeper();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+		void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnBox1BeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
+};
