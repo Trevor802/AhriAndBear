2d640d2f8e06ed44c7d7c1152854b4c828296e41
Joseph Newman <paradoxbuilder@gmail.com>
2020-10-29 15:23:56 -0600

    Resolved compile errors that came in from dev

--- Source/AhriAndBear/Public/Component/AABSurvivalComponent.h
+++ Source/AhriAndBear/Public/Component/AABSurvivalComponent.h
@@ -32,45 +32,6 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival")
 	float UpdateInterval = 2.0f;
 
-	/*UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Health")
-	float MaxHealth = 100.f;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Survival | Health")
-	float CurrentHealth;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character | Survival | Health")
-	float HealthChangeRate = -1.f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Health")
-	float StartingHealth;
-
-	// Thirst-related stats
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Thirst")
-	float MaxThirst = 100.f;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Survival | Thirst")
-	float CurrentThirst;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character | Survival | Thirst")
-	float ThirstChangeRate = -1.f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Thirst")
-	float StartingThirst;
-
-	// Hunger-related stats
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Hunger")
-	float MaxFullness = 100.f;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Survival | Hunger")
-	float CurrentFullness;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character | Survival | Hunger")
-	float FullnessChangeRate = -1.f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Hunger")
-	float StartingFullness;
-
-	// Warmth-related stats
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Warmth")
-	float MaxWarmth = 100.f;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Survival | Warmth")
-	float CurrentWarmth;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character | Survival | Warmth")
-	float WarmthChangeRate = -1.f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character | Survival | Warmth")
-	float StartingWarmth;*/
-
 	UFUNCTION()
 	void UpdateStats(float deltaTime);
 
@@ -82,6 +43,6 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	FORCEINLINE FSurvivalData GetSurvivalData() const { return FSurvivalData{ CurrentHealth, CurrentFullness, CurrentThirst, CurrentWarmth }; }
+	FORCEINLINE FSurvivalData GetSurvivalData() const { return FSurvivalData{ Health.CurrentValue, Hunger.CurrentValue, Thirst.CurrentValue, Warmth.CurrentValue }; }
 	void AddSurvivalData(const FSurvivalData& value);
 };
