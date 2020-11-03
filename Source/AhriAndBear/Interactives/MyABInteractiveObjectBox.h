6baa6b6c0676b2c80d168f0fd2deb502dce634af
Xiubo Zhao <906672365@qq.com>
2020-11-04 00:26:07 +0800

    [PPP] Implementing C++ version

---
+++ Source/AhriAndBear/Interactives/MyABInteractiveObjectBox.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactives/ABInteractiveObjectBase.h"
+#include "MyABInteractiveObjectBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AHRIANDBEAR_API AMyABInteractiveObjectBox : public AABInteractiveObjectBase
+{
+	GENERATED_BODY()
+	
+};
