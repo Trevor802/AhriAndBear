8e42d53b6809740c6d61687e8ac06555568d1d00
Joseph Newman <paradoxbuilder@gmail.com>
2020-11-04 15:07:47 -0700

    Cleaned up header file

--- Source/AhriAndBear/Environments/ABRestArea.cpp
+++ Source/AhriAndBear/Environments/ABRestArea.cpp
@@ -1,14 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "ABRestArea.h"
+#include "Components/BoxComponent.h"
+#include "ABAnimalCharacter.h"
 
 // Sets default values
 AABRestArea::AABRestArea()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	PrimaryActorTick.bCanEverTick = false;
+	//Collider->OnComponentBeginOverlap.AddDynamic(this, &AABRestArea::OnActorBeginOverlap);
+	//Collider->OnComponentEndOverlap.AddDynamic(this )
 }
 
 // Called when the game starts or when spawned
@@ -18,10 +20,10 @@ void AABRestArea::BeginPlay()
 	
 }
 
-// Called every frame
-void AABRestArea::Tick(float DeltaTime)
+void AABRestArea::OnActorBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	Super::Tick(DeltaTime);
+	auto animal = Cast<AABAnimalCharacter>(OtherActor);
+	if (animal == nullptr) return;
 
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Entered rest area"));
 }
-
--- Source/AhriAndBear/Environments/ABRestArea.h
+++ Source/AhriAndBear/Environments/ABRestArea.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Components/BoxComponent.h"
 #include "ABRestArea.generated.h"
 
 // Forward declares
@@ -25,8 +24,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+private:
+	void OnActorBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
 };
