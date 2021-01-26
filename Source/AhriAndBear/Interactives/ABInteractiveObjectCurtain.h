760462c45e4d87ac27a8e9960edf6450362d33a1
jingyi zhang <jerryzhangjingyi@gmail.com>
2021-01-26 10:30:30 -0700

    update garage door

---
+++ Source/AhriAndBear/Interactives/ABInteractiveObjectCurtain.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactives/OccupyingInteractive.h"
+#include "ABInteractiveObjectCurtain.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AHRIANDBEAR_API AABInteractiveObjectCurtain : public AOccupyingInteractive
+{
+	GENERATED_BODY()
+	
+};
