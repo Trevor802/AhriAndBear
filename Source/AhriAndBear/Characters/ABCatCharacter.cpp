3253839a3fffb4aa81e803c70321ea42a0aaed5c
Xiubo Zhao <906672365@qq.com>
2021-01-30 15:18:38 +0800

    Cat night vision reworked & some other half-works

--- Source/AhriAndBear/Characters/ABCatCharacter.cpp
+++ Source/AhriAndBear/Characters/ABCatCharacter.cpp
@@ -7,7 +7,6 @@
 
 #include "Components/PawnNoiseEmitterComponent.h"
 #include "Perception/PawnSensingComponent.h"
-#include "Components/PostProcessComponent.h"
 #include "Camera/CameraComponent.h"
 #include "Engine/Scene.h"
 
@@ -46,24 +45,44 @@ void AABCatCharacter::Tick(float DeltaTime)
 void AABCatCharacter::UseAbility()
 {
 	Super::UseAbility();
-
-	//UE_LOG(LogTemp, Warning, TEXT("???"));
-	abilityOn = true;
 	FPostProcessSettings NVsetting;
-	NVsetting.bOverride_AutoExposureMinBrightness = true;
-	NVsetting.bOverride_AutoExposureMaxBrightness = true;
-	NVsetting.AutoExposureMinBrightness = 0.1f;
-	NVsetting.AutoExposureMaxBrightness = 0.1f;
+	if (!abilityOn)
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("???"));
+		abilityOn = true;
+		normalSettings = camera->PostProcessSettings;
 
-	camera->PostProcessSettings = NVsetting;
-	AbilityEnd();
+		NVsetting.bOverride_VignetteIntensity = true;
+		NVsetting.bOverride_GrainJitter = true;
+		NVsetting.bOverride_GrainIntensity = true;
+		NVsetting.bOverride_WhiteTemp = true;
+		NVsetting.bOverride_WhiteTint = true;
+		NVsetting.bOverride_ColorSaturation = true;
+		NVsetting.bOverride_ColorOffset = true;
+
+		NVsetting.VignetteIntensity = 1.1;
+		NVsetting.GrainJitter = 0.0;
+		NVsetting.GrainIntensity = 0.476191;
+		NVsetting.WhiteTemp = 6900;
+		NVsetting.WhiteTint = -0.6;
+		NVsetting.ColorSaturation = FVector4(1.0, 0.280875, 0.702882, 1.0);
+		NVsetting.ColorOffset = FVector4(-0.185722, 0.0, -0.008505, 0.0);
+		//NVsetting.bOverride_AutoExposureMinBrightness = true;
+		//NVsetting.bOverride_AutoExposureMaxBrightness = true;
+		//NVsetting.AutoExposureMinBrightness = 0.1f;
+		//NVsetting.AutoExposureMaxBrightness = 0.1f;
+
+		camera->PostProcessSettings = NVsetting;
+	}
+	else
+		AbilityEnd();
 }
 
 void AABCatCharacter::AbilityEnd()
 {
 	//UE_LOG(LogTemp, Warning, TEXT("???"));
-	//abilityOn = false;
-	camera->PostProcessSettings.AutoExposureMaxBrightness = 1.0f;
+	abilityOn = false;
+	//camera->PostProcessSettings.AutoExposureMaxBrightness = 1.0f;
 	/*FPostProcessSettings NormalSetting;
 	NormalSetting.bOverride_AutoExposureMinBrightness = true;
 	NormalSetting.bOverride_AutoExposureMaxBrightness = true;
@@ -71,4 +90,5 @@ void AABCatCharacter::AbilityEnd()
 	NormalSetting.AutoExposureMaxBrightness = 8.0f;
 
 	camera->PostProcessSettings = NormalSetting;*/
+	camera->PostProcessSettings = normalSettings;
 }
---
+++ Source/AhriAndBear/Environments/ABScentSource.h
@@ -0,0 +1,40 @@
+ // The improved scent trail that is supposed to be like Scoutflies in Monster Hunter World
+// The source of scent.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ABScentSource.generated.h"
+
+class ABScentIndicator;
+class AABDogCharacter;
+class USphereComponent;
+
+UCLASS()
+class AHRIANDBEAR_API AABScentSource : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AABScentSource();
+	bool GetIsDogInRange();
+	void SetDogInRange(bool);
+	UFUNCTION()
+	void PlayerCharacterInRange(AActor* self, AActor* OtherActor);
+	UFUNCTION()
+	void PlayerCharacterOutRange(AActor* self, AActor* OtherActor);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+		USphereComponent* scentRangeSphere;
+
+private:
+	AABDogCharacter* dogInRange;
+	bool isDogInRange;
+};
