1a5957d9a969c0abfb4a7d46383ba33f41eb74eb
Xiubo Zhao <906672365@qq.com>
2021-01-31 13:49:17 +0800

    Scent tracker collision detection - in progress

--- Source/AhriAndBear/Environments/ABScentIndicator.cpp
+++ Source/AhriAndBear/Environments/ABScentIndicator.cpp
@@ -1,6 +1,9 @@
 
 #include "ABScentIndicator.h"
 #include "Components/SphereComponent.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "CollisionQueryParams.h"
 
 // Sets default values
 AABScentIndicator::AABScentIndicator()
@@ -8,6 +11,7 @@ AABScentIndicator::AABScentIndicator()
 	sensor = CreateDefaultSubobject<USphereComponent>(TEXT("Unit Sensor"));
 	sensor->SetupAttachment(RootComponent);
 	PrimaryActorTick.bCanEverTick = true;
+	targetPosition = FVector::ZeroVector;
 }
 
 // Called when the game starts or when spawned
@@ -21,6 +25,36 @@ void AABScentIndicator::BeginPlay()
 void AABScentIndicator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	MoveToTarget(DeltaTime);
 
+	for (int i = 0; i < helper.G_S_directions.Num(); i++)
+	{
+		DrawDebugLine(GetWorld(),
+			GetActorLocation(),
+			GetActorLocation() + helper.G_S_directions[i] * 10,
+			FColor::Red);
+	}
+	
 }
 
+void AABScentIndicator::MoveToTarget(float DeltaTime)
+{
+	FVector toTarget = targetPosition - GetActorLocation();
+	FVector arrival = FVector::ZeroVector;
+	FVector avoid = FVector::ZeroVector;
+	arrival = toTarget.GetSafeNormal();
+	for (int i = 0; i < helper.G_S_directions.Num(); i++)
+	{
+		FHitResult onHit;
+		if (ActorLineTraceSingle(onHit,
+			GetActorLocation(),
+			GetActorLocation() + helper.G_S_directions[i] * 10,
+			ECollisionChannel::ECC_PhysicsBody,
+			FCollisionQueryParams::DefaultQueryParam))
+		{
+			avoid += -helper.G_S_directions[i].GetSafeNormal();
+		}
+	}
+	myVelocity = (arrival + avoid).GetSafeNormal() * 100;
+	SetActorLocation(GetActorLocation() + myVelocity * DeltaTime);
+}
--- Source/AhriAndBear/Environments/ABScentIndicator.h
+++ Source/AhriAndBear/Environments/ABScentIndicator.h
@@ -3,10 +3,35 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Math/UnrealMathUtility.h"
 #include "ABScentIndicator.generated.h"
 
 class USphereComponent;
 class AABScentSource;
+
+// Stolen from Trevor's research project
+static class BoidHelpler
+{
+public:
+	TArray<FVector> G_S_directions;
+	BoidHelpler()
+	{
+		float goldenRatio = (1 + sqrt(5)) / 2;
+		float _1 = 1.f / num_Directions * 2.f;
+		float angleInc = PI * 2 * goldenRatio;
+		for (int i = 0; i < num_Directions; i++)
+		{
+			float pitch = acosf(1 - i * _1);
+			float roll = angleInc * i;
+			float x = sinf(pitch) * cosf(roll);
+			float y = sinf(pitch) * sinf(roll);
+			float z = cosf(pitch);
+			G_S_directions.Add(FVector(x, y, z));
+		}
+	}
+private:
+	const int num_Directions = 20;
+};
 
 UCLASS()
 class AHRIANDBEAR_API AABScentIndicator : public AActor
@@ -15,17 +40,21 @@ class AHRIANDBEAR_API AABScentIndicator : public AActor
 	
 public:	
 	AABScentIndicator();
-
+	virtual void Tick(float DeltaTime) override;
+	void MoveToTarget(float DeltaTime);
 
 protected:
 	virtual void BeginPlay() override;
 
 public:	
-	virtual void Tick(float DeltaTime) override;
+
 
 private:
 	USphereComponent* sensor;
 	FVector targetPosition;
 	FVector myVelocity;
 	FVector mySteeringForce;
+	BoidHelpler helper;
 };
+
+
