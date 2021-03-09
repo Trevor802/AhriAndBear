45fb711ac253177c175c21cefeb79493ea78f4c9
Joseph Newman <paradoxbuilder@gmail.com>
2021-03-09 10:39:16 -0700

    Added wrapper methods for UE's sound functions

---
+++ Source/AhriAndBear/Private/ABSoundSystemFacade.cpp
@@ -0,0 +1,167 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABSoundSystemFacade.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "../GameBase/UserSettings.h"
+#include "../AhriAndBearGameModeBase.h"
+
+UAudioComponent* UABSoundSystemFacade::ABSpawnSound2D(
+	EAccessibilitySoundType soundType,
+	USoundBase* sound,
+	float volumeMultiplier,
+	float pitchMultiplier,
+	float startTime,
+	USoundConcurrency* soundConcurrency,
+	bool persistAcrossLevelTransition,
+	bool autoDestroy
+)
+{
+	float actualVolume = volumeMultiplier * GetMixedVolumeBySoundType(soundType, volumeMultiplier);
+
+	UAudioComponent* spawnedSound = UGameplayStatics::SpawnSound2D(
+		GEngine->GetWorld(),
+		sound,
+		actualVolume,
+		pitchMultiplier,
+		startTime,
+		soundConcurrency,
+		persistAcrossLevelTransition,
+		autoDestroy
+	);
+
+	return spawnedSound;
+}
+
+UAudioComponent* UABSoundSystemFacade::ABSpawnSoundAtLocation(EAccessibilitySoundType soundType, USoundBase* sound, const FVector& location, const FRotator& rotation, float volumeMultiplier, float pitchMultiplier, float startTime, USoundAttenuation* attenuation, USoundConcurrency* soundConcurrency, bool bAutoDestroy)
+{
+	float actualVolume = GetMixedVolumeBySoundType(soundType, volumeMultiplier);
+	return UGameplayStatics::SpawnSoundAtLocation(
+		GEngine->GetWorld(),
+		sound,
+		location,
+		rotation,
+		actualVolume,
+		pitchMultiplier,
+		startTime,
+		attenuation,
+		soundConcurrency,
+		bAutoDestroy
+	);
+}
+
+UAudioComponent* UABSoundSystemFacade::ABSpawnSoundAttached(EAccessibilitySoundType soundType, USoundBase* sound, USceneComponent* attachToComponent, FName AttachPointName, FVector Location, FRotator Rotation, EAttachLocation::Type LocationType, bool bStopWhenAttachedToDestroyed, float volumeMultiplier, float pitchMultiplier, float startTime, USoundAttenuation* AttenuationSettings, USoundConcurrency* concurrencySettings, bool bAutoDestroy)
+{
+	float actualVolume = GetMixedVolumeBySoundType(soundType, volumeMultiplier);
+	return UGameplayStatics::SpawnSoundAttached(
+		sound,
+		attachToComponent,
+		AttachPointName,
+		Location,
+		Rotation,
+		LocationType,
+		bStopWhenAttachedToDestroyed,
+		actualVolume,
+		pitchMultiplier,
+		startTime,
+		AttenuationSettings,
+		concurrencySettings,
+		bAutoDestroy
+	);
+}
+
+void UABSoundSystemFacade::ABPlayDialogue2D(const UObject* WorldContextObject, UDialogueWave* Dialogue, const FDialogueContext& Context, float volumeMultiplier, float PitchMultiplier, float StartTime, EAccessibilitySoundType soundType)
+{
+	float actualVolume = GetMixedVolumeBySoundType(soundType, volumeMultiplier);
+	UGameplayStatics::PlayDialogue2D(
+		WorldContextObject,
+		Dialogue,
+		Context,
+		actualVolume,
+		PitchMultiplier,
+		StartTime
+	);
+}
+
+void UABSoundSystemFacade::ABPlayDialogueAtLocation(const UObject* WorldContextObject, UDialogueWave* Dialogue, const FDialogueContext& Context, FVector Location, FRotator Rotation, float volumeMultiplier, float pitchMultiplier, float startTime, USoundAttenuation* attenuation, EAccessibilitySoundType soundType)
+{
+	float actualVolume = GetMixedVolumeBySoundType(soundType, volumeMultiplier);
+	UGameplayStatics::PlayDialogueAtLocation(
+		WorldContextObject,
+		Dialogue,
+		Context,
+		Location,
+		Rotation,
+		actualVolume,
+		pitchMultiplier,
+		startTime,
+		attenuation
+	);
+}
+
+void UABSoundSystemFacade::ABPlaySound2D(EAccessibilitySoundType soundType, const UObject* WorldContextObj, USoundBase* Sound, float volumeMultiplier, float pitchMultiplier, float startTime, USoundConcurrency* Concurrency, AActor* OwningActor, bool bIsUISound)
+{
+	float actualVolume = GetMixedVolumeBySoundType(soundType, volumeMultiplier);
+	UGameplayStatics::PlaySound2D(
+		WorldContextObj,
+		Sound,
+		actualVolume,
+		pitchMultiplier,
+		startTime,
+		Concurrency,
+		OwningActor,
+		bIsUISound
+	);
+}
+
+void UABSoundSystemFacade::ABPlaySoundAtLocation(EAccessibilitySoundType soundType, const UObject* WorldContextObject, USoundBase* Sound, FVector Location, FRotator Rotation, float VolumeMultiplier, float PitchMultiplier, float StartTime, USoundAttenuation* AttenuationSettings, USoundConcurrency* ConcurrencySettings, AActor* OwningActor)
+{
+	float actualVolume = GetMixedVolumeBySoundType(soundType, VolumeMultiplier);
+	UGameplayStatics::PlaySoundAtLocation(
+		WorldContextObject,
+		Sound,
+		Location,
+		Rotation,
+		actualVolume,
+		PitchMultiplier,
+		StartTime,
+		AttenuationSettings,
+		ConcurrencySettings,
+		OwningActor
+	);
+}
+
+// vvvvvvvvvvvvvvvvvvvvvv
+// Private static methods
+// ^^^^^^^^^^^^^^^^^^^^^^
+UUserSettings* UABSoundSystemFacade::GetVolumeSettings()
+{
+	AActor* gameModeActor = UGameplayStatics::GetActorOfClass(GEngine->GetWorld(), AAhriAndBearGameModeBase::StaticClass());
+	if (!gameModeActor) {
+		return nullptr;
+	}
+	AAhriAndBearGameModeBase* gameMode = Cast<AAhriAndBearGameModeBase>(gameModeActor);
+	if (!gameMode) return nullptr;
+
+	return gameMode->userSettings;
+}
+
+float UABSoundSystemFacade::GetMixedVolumeBySoundType(EAccessibilitySoundType st, float inVolume)
+{
+	auto userSettings = GetVolumeSettings();
+	if (!userSettings) {
+		// Let's make sure to report an error here
+		return 1.f;
+	}
+	float masterVolume = userSettings->MasterVolume * inVolume;
+
+	switch (st) {
+	case EAccessibilitySoundType::Music:
+		return userSettings->MusicVolume * masterVolume;
+	case EAccessibilitySoundType::SFX:
+		return userSettings->SoundEffectVolume * masterVolume;
+	default:
+		return 1.f;
+	}
+}
---
+++ Source/AhriAndBear/Public/ABSoundSystemFacade.h
@@ -0,0 +1,238 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Sound/DialogueWave.h"
+#include "ABSoundSystemFacade.generated.h"
+
+class UUserSettings;
+
+/**
+ * Identifies the type of sound that should be played.
+ */
+UENUM(BlueprintType)
+enum class EAccessibilitySoundType : uint8 {
+	Music,
+	SFX,
+	Dialogue,
+};
+
+/**
+ * Provides sound methods that automatically mix the volumes properly.
+ */
+UCLASS()
+class AHRIANDBEAR_API UABSoundSystemFacade : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	/**
+	 * Spawns a 2D sound using Ahri and Bear's accessibility settings.
+	 * 
+	 * @see UGameplayStatics::SpawnSound2D
+	 * 
+	 * @param soundType
+	 *   The type of sound that this item is.
+	 * @param sound
+	 *	 The sound to spawn.
+	 * @param volumeMultiplier
+	 *   The volume to multiply the sound by. This will be multiplied by the settings for this sound type.
+	 * @param pitchMultiplier
+	 *   Multiplier by which to raise or lower the pitch.
+	 * @param startTime
+	 *   How far into the sound clip to start playing.
+	 * @param soundConcurrency
+	 *   The concurrency settings for this sound.
+	 * @param persistAcrossLevelTransition
+	 *   Should the sound exist across level transitions?
+	 * @param autoDestroy
+	 *   Should the returned sound component destroy itself?
+	 *
+	 * @return
+	 *   The spawned sound.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Sound")
+	static UAudioComponent* ABSpawnSound2D(
+		EAccessibilitySoundType soundType,
+		USoundBase* sound,
+		float volumeMultiplier = 1.f,
+		float pitchMultiplier = 1.f,
+		float startTime = 0.f,
+		USoundConcurrency* soundConcurrency = nullptr,
+		bool persistAcrossLevelTransition = false,
+		bool autoDestroy = true
+	);
+
+	/**
+	 * Spawns a sound at a location using Ahri and Bear's accessibility settings.
+	 *
+	 * @see UGameplayStatics::SpawnSoundAtLocation
+	 *
+	 * @param soundType
+	 *   The type of sound that this item is.
+	 * @param sound
+	 *	 The sound to spawn.
+	 * @param location
+	 *   The location to spawn the sound at.
+	 * @param rotation
+	 *   The rotation to apply to the sound.
+	 * @param volumeMultiplier
+	 *   The volume to multiply the sound by. This will be multiplied by the settings for this sound type.
+	 * @param pitchMultiplier
+	 *   Multiplier by which to raise or lower the pitch.
+	 * @param startTime
+	 *   How far into the sound clip to start playing.
+	 * @param soundConcurrency
+	 *   The concurrency settings for this sound.
+	 * @param persistAcrossLevelTransition
+	 *   Should the sound exist across level transitions?
+	 * @param autoDestroy
+	 *   Should the returned sound component destroy itself?
+	 *
+	 * @return
+	 *   The spawned sound.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Sound")
+	static UAudioComponent* ABSpawnSoundAtLocation(
+		EAccessibilitySoundType soundType,
+		USoundBase* sound,
+		const FVector& location,
+		const FRotator& rotation = FRotator::ZeroRotator,
+		float volumeMultiplier = 1.f,
+		float pitchMultiplier = 1.f,
+		float startTime = 0.f,
+		USoundAttenuation* attenuation = nullptr,
+		USoundConcurrency* soundConcurrency = nullptr,
+		bool bAutoDestroy = true
+	);
+
+	/**
+	 * Spawns a sound at attached to an actor using Ahri and Bear's accessibility settings.
+	 *
+	 * @see UGameplayStatics::SpawnSoundAttached
+	 *
+	 * @param soundType
+	 *   The type of sound that this item is.
+	 * @param sound
+	 *	 The sound to spawn.
+	 * @param attachToComponent
+	 *   The scene component to attach the sound to.
+	 * @param AttachPointName
+	 *   The name of the point to attach the sound to.
+	 * @param location
+	 *   The location to spawn the sound at.
+	 * @param rotation
+	 *   The rotation to apply to the sound.
+	 * @param LocationType
+	 *   The type of attach location.
+	 * @param volumeMultiplier
+	 *   The volume to multiply the sound by. This will be multiplied by the settings for this sound type.
+	 * @param pitchMultiplier
+	 *   Multiplier by which to raise or lower the pitch.
+	 * @param startTime
+	 *   How far into the sound clip to start playing.
+	 * @param soundConcurrency
+	 *   The concurrency settings for this sound.
+	 * @param persistAcrossLevelTransition
+	 *   Should the sound exist across level transitions?
+	 * @param autoDestroy
+	 *   Should the returned sound component destroy itself?
+	 *
+	 * @return
+	 *   The spawned sound.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Sound")
+	static UAudioComponent* ABSpawnSoundAttached(
+		EAccessibilitySoundType soundType,
+		USoundBase* sound,
+		USceneComponent* attachToComponent,
+		FName AttachPointName = NAME_None,
+		FVector Location = FVector(ForceInit),
+		FRotator Rotation = FRotator::ZeroRotator,
+		EAttachLocation::Type LocationType = EAttachLocation::KeepRelativeOffset,
+		bool bStopWhenAttachedToDestroyed = false,
+		float volumeMultiplier = 1.f,
+		float pitchMultiplier = 1.f,
+		float startTime = 0.f,
+		USoundAttenuation* AttenuationSettings = nullptr,
+		USoundConcurrency* concurrencySettings = nullptr,
+		bool bAutoDestroy = true
+	);
+
+	UFUNCTION(BlueprintCallable, Category = "Sound")
+	static void ABPlayDialogue2D(
+		const UObject * WorldContextObject,
+		UDialogueWave* Dialogue,
+		const FDialogueContext& Context,
+		float VolumeMultiplier = 1.f,
+		float PitchMultiplier = 1.f,
+		float StartTime = 0.f,
+		EAccessibilitySoundType soundType = EAccessibilitySoundType::Dialogue
+	);
+
+	UFUNCTION(BlueprintCallable, Category = "Sound")
+	static void ABPlayDialogueAtLocation(
+		const UObject* WorldContextObject,
+		UDialogueWave* Dialogue,
+		const FDialogueContext& Context,
+		FVector Location,
+		FRotator Rotation = FRotator::ZeroRotator,
+		float volumeMultiplier = 1.f,
+		float pitchMultiplier = 1.f,
+		float startTime = 0.f,
+		USoundAttenuation* attenuation = nullptr,
+		EAccessibilitySoundType soundType = EAccessibilitySoundType::Dialogue
+	);
+
+	UFUNCTION(BlueprintCallable, Category = "Sound")
+	static void ABPlaySound2D(
+		EAccessibilitySoundType soundType,
+		const UObject* WorldContextObject,
+		USoundBase* Sound,
+		float volumeMultiplier = 1.f,
+		float pitchMultiplier = 1.f,
+		float startTime = 0.f,
+		USoundConcurrency* ConcurrencySettings = nullptr,
+		AActor* OwningActor = nullptr,
+		bool bIsUISound = true
+	);
+
+	UFUNCTION(BlueprintCallable, Category = "Sound")
+		static void ABPlaySoundAtLocation(
+			EAccessibilitySoundType soundType,
+			const UObject* WorldContextObject,
+			USoundBase* Sound,
+			FVector Location,
+			FRotator Rotation = FRotator::ZeroRotator,
+			float VolumeMultiplier = 1.f,
+			float PitchMultiplier = 1.f,
+			float StartTime = 1.f,
+			USoundAttenuation* AttenuationSettings = nullptr,
+			USoundConcurrency* ConcurrencySettings = nullptr,
+			AActor* OwningActor = nullptr
+		);
+
+private:
+
+	/**
+	 * Retrieves user's settings.
+	 * 
+	 * @return
+	 *   The user's settings.
+	 */
+	static UUserSettings* GetVolumeSettings();
+
+	/**
+	 * Gets the volume mixed by the sound type.
+	 * 
+	 * @param st:
+	 *   The type of sound to get the volume for.
+	 * @param inVolume:
+	 *   The base volume for the sound being played.
+	 * 
+	 * @return
+	 *   The final volume for the sound to be played.
+	 */
+	static float GetMixedVolumeBySoundType(EAccessibilitySoundType st, float inVolume);
+};
