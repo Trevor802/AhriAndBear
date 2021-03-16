fc5c58e6f3635c71422997eb789153b4d1844582
Joseph Newman <paradoxbuilder@gmail.com>
2021-03-16 13:01:33 -0600

    Added Accessibility Settings struct

---
+++ Source/AhriAndBear/GameBase/AccessibilitySettings.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameBase/AccessibilitySettings.h"
+
+AccessibilitySettings::AccessibilitySettings()
+{
+}
+
+AccessibilitySettings::~AccessibilitySettings()
+{
+}
